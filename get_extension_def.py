d83da7f2b42fa2ca3baa8ff7d4222d733c38ad3f
Maksim Pastukhov <b3cch4@gmail.com>
2022-06-27 17:47:25 +0700

    Changed get_extension func (9 of 16)

--- get_extension_def.py
+++ get_extension_def.py
@@ -1,15 +1,12 @@
 import os
 from urllib.parse import urlparse, unquote
 
+
 def get_extension(url):
     '''Функция, возвращающая расширение файла'''
-    ext = (
-        os.path.splitext(
-            os.path.split(
-                urlparse(
-                    unquote(url)
-                ).path
-            )[-1]
-        )[-1]
-    )[1:]
-    return ext
+    full_url = unquote(url)
+    path_of_url = urlparse(full_url).path
+    name_of_file = os.path.split(path_of_url)[-1]
+    full_extension = os.path.splitext(name_of_file)[-1]
+    extension = full_extension[1:]
+    return extension
