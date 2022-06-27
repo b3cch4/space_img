69262fa8c4fe835cb8e2e92ffe83bcb2549da5f1
Maksim Pastukhov <b3cch4@gmail.com>
2022-06-27 16:50:47 +0700

    Changed download function (7 of 16)

--- downl_img_to_fold_def.py
+++ downl_img_to_fold_def.py
@@ -3,8 +3,7 @@ import os, requests
 
 def download_images_to_folder(url, path):
     '''Функция загружает фотографии по ссылке, полученной в качестве аргумента <url>'''
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
     with open(path, 'wb') as file:
