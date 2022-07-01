125218fb75bd633a2b97b003c024be928f50c20b
Maksim Pastukhov <b3cch4@gmail.com>
2022-07-01 15:08:35 +0700

    Changed variable (1 of 1)

--- get_extension_def.py
+++ get_extension_def.py
@@ -6,7 +6,7 @@ def get_extension(url):
     '''Функция, возвращающая расширение файла'''
     full_url = unquote(url)
     path_of_url = urlparse(full_url).path
-    full_name_of_file = os.path.split(path_of_url)[-1]
-    full_extension = os.path.splitext(name_of_file)[-1]
+    full_name_of_picture = os.path.split(path_of_url)[-1]
+    full_extension = os.path.splitext(full_name_of_picture)[-1]
     extension = full_extension[1:]
     return extension
