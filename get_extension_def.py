69d42dfdb32e548e9d2b035b17a56e1be2fa318b
Maksim Pastukhov <b3cch4@gmail.com>
2022-07-01 14:58:43 +0700

    Revert "Changed variable (1 of 11)"
    This reverts commit 1346c3af9316e8a14ec165404449d297a60516c0.

--- get_extension_def.py
+++ get_extension_def.py
@@ -6,7 +6,7 @@ def get_extension(url):
     '''Функция, возвращающая расширение файла'''
     full_url = unquote(url)
     path_of_url = urlparse(full_url).path
-    full_name_of_file = os.path.split(path_of_url)[-1]
+    name_of_file = os.path.split(path_of_url)[-1]
     full_extension = os.path.splitext(name_of_file)[-1]
     extension = full_extension[1:]
     return extension
