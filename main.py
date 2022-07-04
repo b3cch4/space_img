93e57a47a9186f69c42bb6cf7445abc826a4d2df
Maksim Pastukhov <b3cch4@gmail.com>
2022-07-04 18:32:23 +0700

    Added open with (11 of 11)

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ async def main():
         for address, dirs, files in os.walk('images'):
             for name in files:
                 async with bot:
-                    await bot.send_document(
-                        chat_id=int(CHAT_ID),
-                        #can't find solution for (15 of 16)
-                        document=open(os.path.join(address, name), 'rb')
-                    )
+                    with open(os.path.join(address, name), 'rb') as img:                    
+                        await bot.send_document(
+                            chat_id=int(CHAT_ID),
+                            document = img
+                        )
         time.sleep(10)
 
 
